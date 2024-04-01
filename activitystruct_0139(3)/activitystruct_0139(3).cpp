cf2a25b9ece5dd1842d3985ba236d2ed150a30e9
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-04-01 18:08:51 +0700

    tampilkan data mahasiswa ke-

--- activitystruct_0139(3)/activitystruct_0139(3).cpp
+++ activitystruct_0139(3)/activitystruct_0139(3).cpp
@@ -17,6 +17,7 @@ struct Mahasiswa {
 int main() {
 	Mahasiswa mhs[3];
 	for (int i = 0; i < 3; i++) {
+		cout << " Data ke - " << (i + 1) << endl;
 		cout << "Nomor Mahasiswa : ";
 		getline(cin, mhs[i].nim);
 		cout << "Nama Mahasiswa : ";
@@ -29,5 +30,11 @@ int main() {
 		cin >> mhs[i].alamat.kota;
 		cout << "Umur Mahasiswa : ";
 		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
+	}
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		cout << "Data Mahasiswa ke- ";
 	}
 }
